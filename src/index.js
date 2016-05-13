2e28fe5313e25dc63061995e923be373510c2b7f
elsehow <yes@cosmopol.is>
2016-05-12 21:06:43 -0700

    fixed npm weirdness

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
 let pathStream = require('./src/dragdrop')()
 //HACK pathStream.log('NEW PATH')
 let keys = require('./test/keys.json')
-let tunes = require('ipfstunes-core')(keys, '/tmp/ipfstunes')
-console.log('tunes', tunes)
-
-pathStream.flatMap(tunes.add)
-
-tunes.on('new-view', console.log)
+let tunes = require('./lib/ipfstunes-core')(keys, '/tmp/ipfstunes')
+console.log('tunes', tunes.kefir)
+pathStream.flatMap(tunes.add).log('added tune')
+//tunes.on('new-view', v => {
+//  console.log(v)
+//})
 // TODO setup ipfstunes-core
 // TODO render view events with yo-yo
